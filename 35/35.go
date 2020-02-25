b342713bbdb1773fd7ab7a6913e7951e415fb97b
josephshih13 <josephshih13@gmail.com>
2020-02-25 10:47:40 +0800

    035

---
+++ 35/35.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func searchInsert(nums []int, target int) int {
+	return sort.SearchInts(nums, target)
+}
+
+func main() {
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
+	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
+	fmt.Println(searchInsert([]int{}, 0))
+}
